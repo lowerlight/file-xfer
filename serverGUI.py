26e247b190336c05d65c68268145f4b90155bd7e
Jeffrey Nursalim <jeffrey.nursalim@gmail.com>
2013-11-26 08:59:56 +0800

    Restructured code to let row and column arg supplied from top. Temporarily disabled scrollbars (not working). Added disconnect, upload_file and download_file (not tested).

--- serverGUI.py
+++ serverGUI.py
@@ -35,109 +35,110 @@ class FTPServerClientApp(tkinter.Frame):
         # Grid Position (row, column)
         # self.grid_pstn={(row:, column: }
 
-        self.create_server_control_frame()
-        self.create_start_button()
-        self.create_stop_button()
+        self.create_server_control_frame(rw=0, cl=0)
 
-        self.create_state_frame()
-        self.create_input_frame()
+        self.create_state_frame(rw=1, cl=0)
+        self.create_input_frame(rw=3, cl=0)
 
-        self.create_remote_dir_button()
-        self.create_connect_button()
+        self.create_connect_button(rw=0, cl=3)
+        self.create_remote_dir_button(rw=1, cl=3)
 
-        self.create_dir_frame()
-        self.create_dir_tree_frame(7, 0, "Local")
-        self.create_push_file_button()
+        self.create_disconnect_button(rw=2, cl=3)
 
-        self.create_dir_tree_frame(7, 4, "Remote")
-        self.create_pull_file_button()
+        self.create_dir_frame(rw=4, cl=0)
+        self.create_dir_tree_frame(rw=7, cl=0, tit="Local")
+        # self.create_push_file_button(rw=7, cl=2)
 
-        self.create_browse_button()
-        self.create_share_button()
+        self.create_dir_tree_frame(rw=7, cl=4, tit="Remote")
+        # self.create_pull_file_button(rw=7, cl=3)
 
-        # self.create_stdout_frame()
-        # self.create_stderr_frame()
+        self.create_browse_button(rw=5, cl=4)
+        self.create_share_button(rw=5, cl=5)
+
+        # self.create_stdout_frame(rw=8, cl=0)
+        # self.create_stderr_frame(rw=8, cl=1)
 
         self.handler = FTPHandler
         self.handler.authorizer = self.authorizer
         self.handler.banner = "FTP Server ver %s is ready" % VERSION #does this work in Python3?
 
-    def create_server_control_frame(self):
+    def create_server_control_frame(self, rw, cl):
         # Server Control Frame
         self.server_control_frame = ttk.Frame(self, relief=constants.SOLID, borderwidth=1)
-        self.server_control_frame.grid(row=0, column=0, columnspan=4)
-        ttk.Label(self.server_control_frame, text="Server Control ").grid(row=0, column=0)
+        self.server_control_frame.grid(row=rw, column=cl, columnspan=4, sticky=constants.W,
+            pady=4, padx=5)
+        ttk.Label(self.server_control_frame, text="Server Control ").grid(row=rw, column=cl)
 
-    def create_start_button(self):
         self.start_button = ttk.Button(self.server_control_frame, text="Start",
             command=self.start_server)
-        self.start_button.grid(row=0, column=2)
+        self.start_button.grid(row=rw, column=cl+1)
 
-    def create_stop_button(self):
         self.stop_button = ttk.Button(self.server_control_frame, text="Stop", state=['disabled'],
                         command=self.stop_server)
-        self.stop_button.grid(row=0, column=3)
+        self.stop_button.grid(row=rw, column=cl+2)
 
-    def create_state_frame(self):
+    def create_state_frame(self, rw, cl):
         # State Frame
         state_frame = ttk.Frame(self, relief=constants.SOLID, borderwidth=1)
-        state_frame.grid(row=1, column=0, columnspan=3)
-        ttk.Label(state_frame, text="Server State").grid(row=1, column=0)
+        state_frame.grid(row=rw, column=cl, columnspan=3, sticky=constants.W, pady=4, padx=5)
+        ttk.Label(state_frame, text="Server State").grid(row=rw, column=cl)
 
         state_value = ttk.Label(state_frame, textvariable=self.current_state, foreground='blue')
-        state_value.grid(row=1, column=2)
+        state_value.grid(row=rw, column=cl+1)
 
-    def create_input_frame(self):
+    def create_input_frame(self, rw, cl):
         self.input_frame = ttk.Frame(self, relief=constants.SOLID, borderwidth=1)
-        self.input_frame.grid(row=3, column=0, columnspan=3)
+        self.input_frame.grid(row=rw, column=cl, columnspan=3, sticky=constants.W, pady=4, padx=5)
 
         # ttk.Label(self.input_frame, text="User Name").grid(row=2, column=0)
         # self.username_input = ttk.Entry(self.input_frame, textvariable=self.username)
-        # self.username_input.grid(row=3, column=0)
+        # self.username_input.grid(row=rw, column=cl)
 
         # ttk.Label(self.input_frame, text="Password").grid(row=2, column=1)
         # self.password_input = ttk.Entry(self.input_frame, textvariable=self.password)
-        # self.password_input.grid(row=3, column=1)
+        # self.password_input.grid(row=rw, column=cl+1)
 
-        ttk.Label(self.input_frame, text="Server Address").grid(row=2, column=2)
+        ttk.Label(self.input_frame, text="Server Address").grid(row=rw, column=cl,
+            sticky=constants.W)
         self.listen_ip_input = ttk.Entry(self.input_frame, textvariable=self.listen_ip)
-        self.listen_ip_input.grid(row=3, column=2)
+        self.listen_ip_input.grid(row=rw+1, column=cl)
 
-        ttk.Label(self.input_frame, text="Server Port").grid(row=2, column=3)
+        ttk.Label(self.input_frame, text="Server Port").grid(row=rw, column=cl+1,
+            sticky=constants.W)
         self.listen_port_input = ttk.Entry(self.input_frame, textvariable=self.listen_port)
-        self.listen_port_input.grid(row=3, column=3)
+        self.listen_port_input.grid(row=rw+1, column=cl+1)
 
-    def create_dir_frame(self):
+    def create_dir_frame(self, rw, cl):
         self.dir_frame = ttk.Frame(self, relief=constants.SOLID, borderwidth=1)
-        self.dir_frame.grid(row=4, column=0, columnspan=3)
-
-        ttk.Label(self.dir_frame, text="Shared Directory").grid(row=4, column=0)
+        self.dir_frame.grid(row=rw, column=cl, columnspan=3, sticky=constants.W, pady=4, padx=5)
+        ttk.Label(self.dir_frame, text="Shared Directory").grid(row=rw, column=cl,
+            sticky=constants.W)
 
         # TODO: Rename this to Local root dir later.
         # Now still cannot imagine how to get the remote root directory and display the tree
         self.root_dir_input = ttk.Entry(self.dir_frame, width=64,
             textvariable=self.root_dir['Local'])
-        self.root_dir_input.grid(row=5, column=0)
+        self.root_dir_input.grid(row=rw+1, column=cl)
 
-    def create_browse_button(self):
+    def create_browse_button(self, rw, cl):
         self.browse_button = ttk.Button(self.dir_frame, text="Browse",
             command=partial(self.select_dir, self.root_dir_tree['Local']))
-        self.browse_button.grid(row=5, column=4)
+        self.browse_button.grid(row=rw, column=cl)
 
-    def create_share_button(self):
+    def create_share_button(self, rw, cl):
         self.share_button = ttk.Button(self.dir_frame, text="Share",
             command=partial(self.share_dir, self.root_dir_tree['Local']))
-        self.share_button.grid(row=5, column=5)
+        self.share_button.grid(row=rw, column=cl)
 
-    def create_push_file_button(self):
-        self.push_file_button = ttk.Button(self.dir_tree_frame['Local'], text="Push >>",
+    def create_push_file_button(self, rw, cl):
+        self.push_file_button = ttk.Button(self, text="Push >>",
             command=self.push_file)
-        self.push_file_button.grid(row=0)
+        self.push_file_button.grid(row=rw, column=cl)
 
-    def create_pull_file_button(self):
-        self.pull_file_button = ttk.Button(self.dir_tree_frame['Remote'], text="Pull <<",
+    def create_pull_file_button(self, rw, cl):
+        self.pull_file_button = ttk.Button(self, text="Pull <<",
             command=self.pull_file)
-        self.pull_file_button.grid(row=0)
+        self.pull_file_button.grid(row=rw, column=cl)
 
     def connect(self):
         user = self.username.get()
@@ -160,24 +161,41 @@ class FTPServerClientApp(tkinter.Frame):
         self.root_dir_tree['Remote'].root_directory.set(str(os.sep))
         self.list_remote_dir()
 
-    def create_remote_dir_button(self):
+    def disconnect(self):
+        user = self.username.get()
+        pswd = self.password.get()
+        host = self.listen_ip.get()
+        port = int(self.listen_port.get())
+
+        try:
+             self.ftp_conn.quit()
+        except:
+            print("Already disconnected")
+
+    def create_remote_dir_button(self, rw, cl):
         self.remote_dir_button = ttk.Button(self, text="Remote Dir",
             command=self.list_remote_dir)
-        self.remote_dir_button.grid(row=0, column=2)
+        self.remote_dir_button.grid(row=rw, column=cl)
 
-    def create_connect_button(self):
+    def create_connect_button(self, rw, cl):
         self.connect_button = ttk.Button(self, text="Connect", command=self.connect)
-        self.connect_button.grid(row=0, column=3)
+        self.connect_button.grid(row=rw, column=cl)
+
+    def create_disconnect_button(self, rw, cl):
+        self.connect_button = ttk.Button(self, text="Disconnect", command=self.disconnect)
+        self.connect_button.grid(row=rw, column=cl)
 
     def upload_file(filename, outfile=None):
         if not outfile:
             outfile = sys.stdout
         if re.search('\.txt$', filename):
-            self.ftp_conn.storlines("STOR " + filename, open(filename))
+            self.ftp_conn.storlines("STOR " + filename, open(filename),
+                callback=lambda : print('.'))
         else:
-            self.ftp_conn.storbinary("STOR " + filename, open(filename, "rb"), 1024)
+            self.ftp_conn.storbinary("STOR " + filename, open(filename, "rb"), 1024,
+                lambda : print('.'))
 
-    def get_file(filename, outfile=None):
+    def download_file(filename, outfile=None):
         if not outfile:
             outfile = sys.stdout
         if re.search('\.txt$', filename):
@@ -203,27 +221,26 @@ class FTPServerClientApp(tkinter.Frame):
 
     def push_file(self):
         files = self.root_dir_tree['Local'].selection()
-        for fil in files:
+        for filename in files:
             print(fil)
-            # self.upload_file(conn, file)
+            self.upload_file(filename)
 
     def pull_file(self):
         files = self.root_dir_tree['Remote'].selection()
-        for fil in files:
-            print(fil)
-            # self.getfile(conn, file)
+        for filename in files:
+            print(filename)
+            self.download_file(filename, "out_{0}".format(filename))
 
     def create_dir_tree_frame(self, rw, cl, tit):
         self.dir_tree_frame[tit] = ttk.Frame(self, relief=constants.SOLID, borderwidth=1)
-        self.dir_tree_frame[tit].grid(row=rw, column=cl, rowspan=4, columnspan=4)
-        ttk.Label(self.dir_tree_frame[tit], text=tit).grid(row=max(0,rw-1), column=0)
-
         self.root_dir_tree[tit] = RootTree(self, columns=('fullpath','type','size'),
             displaycolumns='size', root_dir=self.root_dir[tit],
             conn=self.ftp_conn if tit=='Remote' else None)
         print(tit, self.root_dir_tree[tit])
-        yScrollBar = ttk.Scrollbar(orient=constants.VERTICAL, command=self.root_dir_tree[tit].yview)
-        xScrollBar = ttk.Scrollbar(orient=constants.HORIZONTAL,
+
+        yScrollBar = ttk.Scrollbar(self.root_dir_tree[tit], orient=constants.VERTICAL,
+            command=self.root_dir_tree[tit].yview)
+        xScrollBar = ttk.Scrollbar(self.root_dir_tree[tit], orient=constants.HORIZONTAL,
             command=self.root_dir_tree[tit].xview)
         self.root_dir_tree[tit]['yscroll'] = yScrollBar.set
         self.root_dir_tree[tit]['xscroll'] = xScrollBar.set
@@ -232,29 +249,35 @@ class FTPServerClientApp(tkinter.Frame):
         self.root_dir_tree[tit].heading('size', text='Size', anchor=constants.W)
         self.root_dir_tree[tit].column('size', stretch=0, width=40)
 
-        self.root_dir_tree[tit].grid(row=rw+1, column=cl, sticky=constants.NSEW)
-        yScrollBar.grid(row=rw+1, column=cl+1, sticky=constants.NS)
-        xScrollBar.grid(row=rw+2, column=cl, sticky=constants.EW)
+        self.dir_tree_frame[tit].grid(row=rw, column=cl, sticky=constants.W, pady=4, padx=5)
+        ttk.Label(self.dir_tree_frame[tit], text=tit).grid(row=rw, column=cl, sticky=constants.W)
+        self.root_dir_tree[tit].grid(in_=self.dir_tree_frame[tit], row=rw+1, column=cl,
+            sticky=constants.NSEW)
+        # yScrollBar.grid(row=rw+1, column=cl+3, sticky=constants.NS)
+        # xScrollBar.grid(row=rw+3, column=cl, sticky=constants.EW)
+        # set frame resizing priorities
+        # self.dir_tree_frame[tit].rowconfigure(0, weight=1)
+        # self.dir_tree_frame[tit].columnconfigure(0, weight=1)
 
     # Enable this frame later
-    # def create_stdout_frame(self):
+    # def create_stdout_frame(self, rw, cl):
     #     self.stdout_frame = ttk.Frame(self, relief=constants.SOLID, borderwidth=1)
-    #     self.stdout_frame.grid(row=12, column=0)
+    #     self.stdout_frame.grid(row=rw, column=cl)
     #
     #     self.old_stdout = sys.stdout
     #     self.text = tkinter.Text(self, width=40, height=10, wrap='none')
-    #     self.text.grid(row=13, column=0)
+    #     self.text.grid(row=rw+1, column=cl)
     #     sys.stdout = StdoutRedirector(self.text)
 
     # Enable this frame later
-    # def create_stderr_frame(self):
+    # def create_stderr_frame(self, rw, cl):
     #     self.stderr_frame = ttk.Frame(self, relief=constants.SOLID, borderwidth=1)
-    #     self.stderr_frame.grid(row=12, column=1)
+    #     self.stderr_frame.grid(row=rw, column=cl)
     #
     #     self.old_stderr = sys.stderr
     #
     #     self.err = tkinter.Text(self, width=60, height=10, wrap='none')
-    #     self.err.grid(row=13, column=1)
+    #     self.err.grid(row=rw+1, column=cl+1)
     #     sys.stderr = StdoutRedirector(self.err)
 
     def initialise(self):
@@ -269,7 +292,7 @@ class FTPServerClientApp(tkinter.Frame):
         self.root_dir['Local'].set(os.getcwd() + os.sep)
 
         self.current_state = tkinter.StringVar()
-        self.current_state.set("not running")
+        self.current_state.set("NOT RUNNING")
 
         self.listen_ip = tkinter.StringVar()
         self.listen_ip.set("127.0.0.1")
